66edb27a5fce452335fe27ad12a70c2531abf082
skynetsites <isaiaswebnet@gmail.com>
2025-05-18 07:57:46 -0300

    Updates

--- assets/card.esm-C63l5HcM.js
+++ assets/card.esm-C63l5HcM.js
@@ -1,4 +1,4 @@
-import{a as t}from"./index-Bjb-LFVA.js";import{u as S,P as J,C as X,a as H,O as l,c as M}from"./App-DdZsoVxF.js";var O={root:"p-card p-component",header:"p-card-header",title:"p-card-title",subTitle:"p-card-subtitle",content:"p-card-content",footer:"p-card-footer",body:"p-card-body"},U=`
+import{a as t}from"./index-oSvAuNrD.js";import{u as S,P as J,C as X,a as H,O as l,c as M}from"./App-B0lMC-8v.js";var O={root:"p-card p-component",header:"p-card-header",title:"p-card-title",subTitle:"p-card-subtitle",content:"p-card-content",footer:"p-card-footer",body:"p-card-body"},U=`
 @layer primereact {
     .p-card-header img {
         width: 100%;
